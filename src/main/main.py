cdf8f6d66e9a0073075d4edc0269fda88b15cd90
YomNom <YomNom@github.com>
2026-08-04 21:29:35 +0000

    Test run: 1/5 passed, 4 failed, 0 skipped - 2026-08-04T21:29:35.894Z

--- src/main/main.py
+++ src/main/main.py
@@ -60,12 +60,9 @@ def load_and_clean_users(file_path):
         f.readline() 
         for line in f:
             l = [item.strip() for item in line.split(",")]
-
             if len(l) == 2:
-                print("TWO: ",l[0], l[1])
-                if (l[0] == ' ' or l[1] == ' '):
+                if (l[0] == '' or l[1] == ' '):
                     continue
-                print(l)
                 cursor.execute(f"INSERT INTO users (firstName,lastName) VALUES ('{l[0]}', '{l[1]}')")
 
 # This function will load the callLogs.csv file into the callLogs table, discarding any records with incomplete data
